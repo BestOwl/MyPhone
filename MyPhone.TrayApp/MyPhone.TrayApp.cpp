db4e51114677ddda1ddee58ad677642ed250ab86
NightOwl <midnightow1@outlook.com>
2020-07-06 20:04:22 +0800

    TrayApp: Added NotifyIcon and ContextMenu

--- MyPhone.TrayApp/MyPhone.TrayApp.cpp
+++ MyPhone.TrayApp/MyPhone.TrayApp.cpp
@@ -1,18 +1,27 @@
-﻿#include <windows.h>
+﻿#include "resource.h"
+
+#include <windows.h>
+#include <windowsx.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <winrt/Windows.system.h>
 #include <winrt/windows.ui.xaml.hosting.h>
 #include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
-#include <winrt/windows.ui.xaml.controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Windows.ui.xaml.media.h>
 #include <winrt/Windows.Foundation.Collections.h>
+#include <winrt\impl\Windows.UI.Xaml.Markup.0.h>
 
 using namespace winrt;
 using namespace Windows::UI;
 using namespace Windows::UI::Composition;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
 using namespace Windows::UI::Xaml::Hosting;
+using namespace Windows::Foundation;
 using namespace Windows::Foundation::Numerics;
 
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
@@ -21,21 +30,32 @@ HWND _hWnd;
 HWND _childhWnd;
 HINSTANCE _hInstance;
 
+Canvas _Root = nullptr;
+MenuFlyout _ContextMenu = nullptr;
+// {E7F3B498-BDDC-4343-AF8E-D8AB405C1232}
+static const GUID _ContextMenuGuid =
+{ 0xe7f3b498, 0xbddc, 0x4343, { 0xaf, 0x8e, 0xd8, 0xab, 0x40, 0x5c, 0x12, 0x32 } };
+bool _ContextMenuMouseClick;
+
+
+NOTIFYICONDATAW _nid;
+NOTIFYICONIDENTIFIER _niid;
+constexpr UINT WM_NOTIFYICON = WM_APP + 1;
+
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
 	_hInstance = hInstance;
 
 	// The main window class name.
-	const wchar_t szWindowClass[] = L"Win32DesktopApp";
+	const wchar_t szWindowClass[] = L"MYPHONETRAYAPP";
 	WNDCLASSEX windowClass = { };
 
 	windowClass.cbSize = sizeof(WNDCLASSEX);
 	windowClass.lpfnWndProc = WindowProc;
 	windowClass.hInstance = hInstance;
 	windowClass.lpszClassName = szWindowClass;
-	windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 
-	windowClass.hIconSm = LoadIcon(windowClass.hInstance, IDI_APPLICATION);
+	windowClass.hIconSm = LoadIcon(windowClass.hInstance, MAKEINTRESOURCEW(IDI_APPICON));
 
 	if (RegisterClassEx(&windowClass) == NULL)
 	{
@@ -43,11 +63,12 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 		return 0;
 	}
 
-	_hWnd = CreateWindow(
+	_hWnd = CreateWindowEx(
+		WS_EX_NOACTIVATE | WS_EX_LAYERED | WS_EX_TOPMOST,
 		szWindowClass,
-		L"Windows c++ Win32 Desktop App",
-		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		L"MyPhone.TrayApp",
+		WS_POPUP,
+		0, 0, 0, 0,
 		NULL,
 		NULL,
 		hInstance,
@@ -59,8 +80,26 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 		return 0;
 	}
 
+	// Init NotifyIcon
+	_nid.cbSize = sizeof(_nid);
+	_nid.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP | NIF_SHOWTIP | NIF_GUID;
+	_nid.uCallbackMessage = WM_NOTIFYICON;
+	_nid.uVersion = NOTIFYICON_VERSION_4;
+	_nid.hIcon = windowClass.hIconSm;
+	_nid.hWnd = _hWnd;
+	_nid.guidItem = _ContextMenuGuid;
+	wcscpy_s(_nid.szTip, L"My Phone");
 
-	//XAML Island section
+	_niid.cbSize = sizeof(_niid);
+	_niid.guidItem = _ContextMenuGuid;
+
+	Shell_NotifyIcon(NIM_ADD, &_nid);
+	// Set the behaviour version, opt-in WM_CONTEXTMENU message
+	Shell_NotifyIcon(NIM_SETVERSION, &_nid); 
+
+
+#pragma region XAMLIsland
+	//https://docs.microsoft.com/en-us/windows/apps/desktop/modernize/host-standard-control-with-xaml-islands-cpp
 
 	// The call to winrt::init_apartment initializes COM; by default, in a multithreaded apartment.
 	winrt::init_apartment(apartment_type::single_threaded);
@@ -68,36 +107,59 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	// Initialize the Xaml Framework's corewindow for current thread
 	WindowsXamlManager winxamlmanager = WindowsXamlManager::InitializeForCurrentThread();
 
-	// This DesktopWindowXamlSource is the object that enables a non-UWP desktop application 
-	// to host UWP controls in any UI element that is associated with a window handle (HWND).
 	DesktopWindowXamlSource desktopSource;
-	// Get handle to corewindow
 	auto interop = desktopSource.as<IDesktopWindowXamlSourceNative>();
-	// Parent the DesktopWindowXamlSource object to current window
 	check_hresult(interop->AttachToWindow(_hWnd));
 
 	// This Hwnd will be the window handler for the Xaml Island: A child window that contains Xaml.  
 	HWND hWndXamlIsland = nullptr;
-	// Get the new child window's hwnd 
 	interop->get_WindowHandle(&hWndXamlIsland);
 	// Update the xaml island window size becuase initially is 0,0
-	SetWindowPos(hWndXamlIsland, 0, 200, 100, 800, 200, SWP_SHOWWINDOW);
+	SetWindowPos(hWndXamlIsland, 0, 0, 0, 1, 1, SWP_SHOWWINDOW);
 
 	//Creating the Xaml content
-	Windows::UI::Xaml::Controls::StackPanel xamlContainer;
+	Canvas xamlContainer;
 	xamlContainer.Background(Windows::UI::Xaml::Media::SolidColorBrush{ Windows::UI::Colors::LightGray() });
 
-	Windows::UI::Xaml::Controls::TextBlock tb;
-	tb.Text(L"Hello World from Xaml Islands!");
-	tb.VerticalAlignment(Windows::UI::Xaml::VerticalAlignment::Center);
-	tb.HorizontalAlignment(Windows::UI::Xaml::HorizontalAlignment::Center);
-	tb.FontSize(48);
+	FontIcon closeIcon;
+	closeIcon.Glyph(L"\xE8BB");
 
-	xamlContainer.Children().Append(tb);
-	xamlContainer.UpdateLayout();
+	MenuFlyoutItem exitItem;
+	exitItem.Text(L"Exit");
+	exitItem.Icon(closeIcon);
+	exitItem.Click([](auto sender, auto args)
+	{
+		PostMessage(_hWnd, WM_CLOSE, 0, 0);
+	});
+
+	MenuFlyoutItem openAppItem;
+	openAppItem.Text(L"Open App");
+
+	MenuFlyout menu;
+	menu.Items().Append(openAppItem);
+	menu.Items().Append(exitItem);
+	menu.Opened([](auto sender, auto args) 
+	{
+		int i = _ContextMenu.Items().Size();
+		if (i > 0)
+		{
+			if (_ContextMenuMouseClick)
+			{
+				_ContextMenu.Items().GetAt(i - 1).Focus(FocusState::Pointer);
+			}
+		}
+		_ContextMenuMouseClick = false;
+	});
+	menu.Closed([](auto sender, auto args)
+	{
+		ShowWindow(_hWnd, SW_HIDE);
+	});
+	_ContextMenu = menu;
+
 	desktopSource.Content(xamlContainer);
+	_Root = xamlContainer;
 
-	//End XAML Island section
+#pragma endregion XAMLIsland
 
 	ShowWindow(_hWnd, nCmdShow);
 	UpdateWindow(_hWnd);
@@ -115,45 +177,35 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT messageCode, WPARAM wParam, LPARAM lParam)
 {
-	PAINTSTRUCT ps;
-	HDC hdc;
-	wchar_t greeting[] = L"Hello World in Win32!";
-	RECT rcClient;
-
 	switch (messageCode)
 	{
-	case WM_PAINT:
-		if (hWnd == _hWnd)
-		{
-			hdc = BeginPaint(hWnd, &ps);
-			TextOut(hdc, 300, 5, greeting, wcslen(greeting));
-			EndPaint(hWnd, &ps);
-		}
-		break;
 	case WM_DESTROY:
+		Shell_NotifyIcon(NIM_DELETE, &_nid);
 		PostQuitMessage(0);
 		break;
+	case WM_NOTIFYICON:
+		switch (LOWORD(lParam))
+		{
+		case WM_RBUTTONUP:
+			_ContextMenuMouseClick = true;
+			break;
+		case WM_CONTEXTMENU:
+			SetForegroundWindow(hWnd);
 
-		//creating main window  
-	case WM_CREATE:
-		_childhWnd = CreateWindowEx(0, L"ChildWClass", NULL, WS_CHILD | WS_BORDER, 0, 0, 0, 0, hWnd, NULL, _hInstance, NULL);
-		return 0;
-		// main window changed size 
-	case WM_SIZE:
-		// Get the dimensions of the main window's client 
-		// area, and enumerate the child windows. Pass the 
-		// dimensions to the child windows during enumeration. 
+			RECT rect;
+			Shell_NotifyIconGetRect(&_niid, &rect);
+			
+			Point point
+			{
+				static_cast<float>(rect.left),
+				static_cast<float>(rect.top)
+			};
 
-		GetClientRect(hWnd, &rcClient);
-		MoveWindow(_childhWnd, 200, 200, 400, 500, TRUE);
-		ShowWindow(_childhWnd, SW_SHOW);
-
-
-		return 0;
-
-		// Process other messages. 
-
-
+			_ContextMenu.ShowAt(_Root, point);
+			break;
+		default:
+			break;
+		}
 	default:
 		return DefWindowProc(hWnd, messageCode, wParam, lParam);
 		break;
