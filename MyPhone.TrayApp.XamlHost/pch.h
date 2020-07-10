adc081779dfdbf5ce17e1d39233d68d4fee6b057
NightOwl <midnightow1@outlook.com>
2020-07-10 19:42:09 +0800

    Temporarially delete WinUI in TrayApp for better debugging

--- MyPhone.TrayApp.XamlHost/pch.h
+++ MyPhone.TrayApp.XamlHost/pch.h
@@ -14,8 +14,8 @@
 #include <winrt/Windows.UI.Xaml.Markup.h>
 #include <winrt/Windows.UI.Xaml.Navigation.h>
 
-#include "winrt/Microsoft.UI.Xaml.Automation.Peers.h"
-#include "winrt/Microsoft.UI.Xaml.Controls.h"
-#include "winrt/Microsoft.UI.Xaml.Controls.Primitives.h"
-#include "winrt/Microsoft.UI.Xaml.Media.h"
-#include "winrt/Microsoft.UI.Xaml.XamlTypeInfo.h"
+//#include "winrt/Microsoft.UI.Xaml.Automation.Peers.h"
+//#include "winrt/Microsoft.UI.Xaml.Controls.h"
+//#include "winrt/Microsoft.UI.Xaml.Controls.Primitives.h"
+//#include "winrt/Microsoft.UI.Xaml.Media.h"
+//#include "winrt/Microsoft.UI.Xaml.XamlTypeInfo.h"
