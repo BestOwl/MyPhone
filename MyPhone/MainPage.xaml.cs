6446e27623c65518e7dffeffe8f3b6a284536168
NightOwl <midnightow1@outlook.com>
2021-09-30 23:04:20 +0800

    Add MessagePage and improve the NavigationView design
    Add MainPageViewModel
    Add Icon assets image

--- MyPhone/MainPage.xaml.cs
+++ MyPhone/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoodTimeStudio.MyPhone.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -26,30 +27,87 @@ namespace GoodTimeStudio.MyPhone
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public MainPageViewModel ViewModel { get; set; }
+
+        public string AppTitleDisplayName { get => Windows.ApplicationModel.Package.Current.DisplayName; }
 
         public MainPage()
         {
             this.InitializeComponent();
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            // Hide default title bar.
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            UpdateTitleBarLayout(coreTitleBar);
+
+            // Set XAML element as a draggable region.
             Window.Current.SetTitleBar(AppTitleBar);
-            CoreApplication.GetCurrentView().TitleBar.LayoutMetricsChanged += (s, e) => UpdateAppTitle(s);
+
+            // Register a handler for when the size of the overlaid caption control changes.
+            // For example, when the app moves to a screen with a different DPI.
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            // Register a handler for when the title bar visibility changes.
+            // For example, when the title bar is invoked in full screen mode.
+            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+
+            //Register a handler for when the window changes focus
+            Window.Current.Activated += Current_Activated;
+
+            ViewModel = new MainPageViewModel();
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            UpdateTitleBarLayout(sender);
+        }
+
+        private void UpdateTitleBarLayout(CoreApplicationViewTitleBar coreTitleBar)
+        {
+            // Update title bar control size as needed to account for system size changes.
+            AppTitleBar.Height = coreTitleBar.Height;
+
+            // Ensure the custom title bar does not overlap window caption controls
+            Thickness currMargin = AppTitleBar.Margin;
+            AppTitleBar.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
+        }
+
+        private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (sender.IsVisible)
+            {
+                AppTitleBar.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        // Update the TitleBar based on the inactive/active state of the app
+        private void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            SolidColorBrush defaultForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorPrimaryBrush"];
+            SolidColorBrush inactiveForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorDisabledBrush"];
+
+            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated)
+            {
+                AppTitle.Foreground = inactiveForegroundBrush;
+            }
+            else
+            {
+                AppTitle.Foreground = defaultForegroundBrush;
+            }
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            NavigationViewControl.SelectedItem = _Tab_Call;
-        }
-
-        public string GetAppTitleFromSystem()
-        {
-            return Windows.ApplicationModel.Package.Current.DisplayName;
-        }
-
-        void UpdateAppTitle(CoreApplicationViewTitleBar coreTitleBar)
-        {
-            //ensure the custom title bar does not overlap window caption controls
-            Thickness currMargin = AppTitleBar.Margin;
-            AppTitleBar.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
+            //NavigationViewControl.SelectedItem = TabCall;
         }
 
         private void NavigationViewControl_SelectionChanged(MUXC.NavigationView sender, MUXC.NavigationViewSelectionChangedEventArgs args)
@@ -58,14 +116,24 @@ namespace GoodTimeStudio.MyPhone
             {
 
             }
-            else if (args.SelectedItem == _Tab_Call)
+            else
             {
-                contentFrame.Navigate(typeof(CallPage));
+                NavigationMenu item = (NavigationMenu)args.SelectedItem; //TODO: better MVVM
+                switch (item.Name)
+                {
+                    case "Call":
+                        contentFrame.Navigate(typeof(CallPage));
+                        break;
+                    case "Message":
+                        contentFrame.Navigate(typeof(MessagePage));
+                        break;
+                    case "Debug":
+                        contentFrame.Navigate(typeof(TestPage));
+                        break;
+                }
             }
-            else if (args.SelectedItem == _Tab_Debug)
-            {
-                contentFrame.Navigate(typeof(TestPage));
-            }
+
         }
+
     }
 }
